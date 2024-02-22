c154d04d0dedaac73423f92ea9d25bbd7abc31e0
venkykumar0006 <65965126+BoddepallyVenkatesh06@users.noreply.github.com>
2024-02-22 23:31:08 +0530

    Tech Blogs

---
+++ components/TheFooter.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="text-center">
+    <p>
+      Made by <a href="https://markodenic.com">Marko Denic</a> with <a href="https://nuxtjs.org">Nuxt.js</a>. Icons from <a href="https://www.freepik.com/">Freepik</a>.
+    </p>
+
+    <p>
+      Marko also made <a href="https://javascriptquiz.com">JavaScript Quiz</a> and <a href="https://freecodetools.org">Free Code Tools</a>.
+    </p>
+
+    <p>
+      Source on <a href="https://github.com/MarkoDenic/awesome-tech-blogs">GitHub</a>. Add yourself!
+    </p>
+
+    <p>
+      Hosted on <a href="https://www.netlify.com/">Netlify</a>.
+    </p>
+
+    <p>
+      Inspired by <a href="https://wesbos.com/">Wes Bos's</a> <a href="https://uses.tech/">uses.tech</a> page.
+    </p>
+  </footer>
+</template>
---
+++ components/TheHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header
+    id="top"
+    class="text-center pb-6"
+  >
+    <GithubCorner />
+
+    <a href="/">
+      <img
+        src="/logo.svg"
+        width="70"
+        height="70"
+        alt="Awesome Tech Blogs"
+      />
+    </a>
+
+    <h1>
+      Tech blogs
+    </h1>
+
+    <p>
+      This is a list of Awesome Tech Blogs.
+    </p>
+  </header>
+</template>
